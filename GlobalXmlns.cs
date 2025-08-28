ddc667fe9ef714afb7e9baadec6ecd46dbf2c55b
njohnb <nicholas.barger.14@gmail.com>
2025-08-28 13:38:42 -0500

    --intital commit

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RecipeOptimizer")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RecipeOptimizer.Pages")]
